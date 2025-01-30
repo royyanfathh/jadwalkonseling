62b5a54fec811baa3074278f60e0f298cbce6c98
Royyan Fathullah <royyanfathullah159@gmail.com>
2025-01-31 01:15:26 +0700

    done setup template

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,13 +12,14 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('username')->unique();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('levels');
+            $table->boolean('is_active')->default(true)->index();
             $table->timestamps();
+            $table->softDeletes()->index();
         });
     }
 
